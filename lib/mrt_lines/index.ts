ceb04f177990b743875fb05c263510ef8ee9b8d1
Emilie Holiday <emilieholiday@Emilies-MacBook-Pro-4.local>
2026-06-30 09:30:16 +0800

    updated readme

--- lib/mrt_lines/index.ts
+++ lib/mrt_lines/index.ts
@@ -1,4 +1,4 @@
 import { EWL_STATIONS, EWL_EDGES } from "./ewl";
 
-export const ALL_STATIONS = [...EWL_STATIONS];
-export const ALL_EDGES = [...EWL_EDGES];
+export const ALL_STATIONS = [EWL_STATIONS];
+export const ALL_EDGES = [EWL_EDGES];
